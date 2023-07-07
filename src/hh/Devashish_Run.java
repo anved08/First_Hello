f3a229a80fd068a6e8682973fe362ce74bcb5faf
anved <anved08@gmail.com>
2023-07-07 17:08:16 +0530

    First Commit Anved08

---
+++ src/hh/Devashish_Run.java
@@ -0,0 +1,39 @@
+package hh;
+
+import java.util.*;
+public class Devashish_Run {//Start of Class
+	public static void main(String args[]){    // Start of Main method
+	
+		
+		
+		for(int i=0;i<10;i++) {
+			System.out.print(" Hello : "+i);
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+		
+		
+		
+		
+		
+		
+		
+	
+	}//End of Main
+}//End of Class
+
